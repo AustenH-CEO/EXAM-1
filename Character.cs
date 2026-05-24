1b4e15facf979db33918db3882c7e371d5508422
MRHOM <MRHOM@LAPTOP-9RSIE69G>
2026-05-23 20:40:44 -0600

    Created main menu

---
+++ Character.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterFile
+{
+    internal class Character
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,66 @@
-﻿
+﻿// This program will create a character manager
+// Austen Hernandez
+// 5/23/26
+
+using System.ComponentModel.Design;
+
+public class Program
+{
+    public static void Main()
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            CharacterMenu(ref exit);
+        }
+        Console.WriteLine("Exiting...");
+    }
+    static bool CharacterMenu(ref bool Exit)
+    {
+        string CharacterClass;
+        do
+        {
+            Console.WriteLine("Choose a class: (1) Warrior, (2) Mage, (3) Rogue, (0) Exit");
+            CharacterClass = Console.ReadLine();
+            if (int.TryParse(CharacterClass, out int input))
+            {
+                switch (input)
+                {
+                    case (int)MenuChoice.Warrior:
+                        Console.WriteLine("Creating Warrior...");
+                        break;
+                    case (int)MenuChoice.Mage:
+                        Console.WriteLine("Creating Mage...");
+                        break;
+                    case (int)MenuChoice.Rogue:
+                        Console.WriteLine("Creating Rogue...");
+                        break;
+                    case (int)MenuChoice.Exit:
+                        Exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Not an option");
+                        break;
+                }
+            }  
+            else
+                Console.WriteLine("Please Enter a Valid Input...");
+        } while (Exit == false);
+
+        return Exit;
+    }
+
+}
+enum FightingClass
+{
+    Warrior = 80,
+    Mage = 45,
+    Rogue = 65
+}
+enum MenuChoice
+{
+    Warrior = 1,
+    Mage = 2,
+    Rogue = 3,
+    Exit = 0
+}
